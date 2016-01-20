2ca4ff8e600aa6f063ea697fbeb6236659492bac
seescode <seesyong@gmail.com>
2016-01-20 11:13:53 -0500

    Add grunt open

--- app/dest/all.js
+++ app/dest/all.js
@@ -13,6 +13,7 @@ angular.module('app', [
 
 		vm.text = '3334';
 	}
+	MainController.$inject = ['$scope', '$location', '$http'];
 
 	angular.module('app').controller('MainController', MainController);
 })();
@@ -24,6 +25,7 @@ angular.module('app', [
 
 		vm.text = '3334';
 	}
+	Main2Controller.$inject = ['$scope', '$location', '$http'];
 
 	angular.module('app').controller('Main2Controller', Main2Controller);
 })();
